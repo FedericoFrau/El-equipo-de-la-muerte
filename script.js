7e4f341481940501525707f0a72f7cd15f14ca61
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-11-22 20:08:32 -0300

    feat/arrayProductos

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-pepita la pistolera
+let producto = [
+	{
+		id: 1,
+		nombre: 'Computadora',
+		descripcion: 'Computadora amd 5 nucleos',
+		precio: 50000,
+	},
+	{
+		id: 2,
+		nombre: 'Laptop',
+		descripcion: 'Laptop amd 5 nucleos',
+		precio: 80000,
+	},
+	{
+		id: 3,
+		nombre: 'Celular',
+		descripcion: 'Celular Samsung',
+		precio: 60000,
+	},
+]
