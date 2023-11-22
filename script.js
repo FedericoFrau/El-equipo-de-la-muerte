37351c718db27d16217ea518ec2b2d7ffa1010ba
Ezequiel Massa <ezequielmassa.dev@gmail.com>
2023-11-22 20:30:37 -0300

    feat/creada funcionalidad eliminar producto por Id

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let producto = [
+let productos = [
 	{
 		id: 1,
 		nombre: 'Computadora',
@@ -18,3 +18,20 @@ let producto = [
 		precio: 60000,
 	},
 ]
+
+const btnEliminarProducto = document.getElementById('btnEliminarProducto')
+
+btnEliminarProducto.addEventListener('click', () => {
+	let id = parseInt(prompt('Ingresa el ID del producto'))
+	eliminarProducto(id)
+})
+function eliminarProducto(id) {
+	let index = productos.findIndex((producto) => producto.id === id)
+	if (index === -1) {
+		alert('No se encontro el producto')
+		return
+	} else {
+		productos = productos.filter((producto) => producto.id !== id)
+		console.log(productos)
+	}
+}
