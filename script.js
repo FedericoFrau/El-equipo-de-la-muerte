103a5ec105d7d863d4c6f37cc57d0759bbe5d24e
Ariana Alderete <alderete.ariana.1406@gmail.com>
2023-11-23 20:53:15 -0300

    commit buscar

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
-//let producto = [
-	//{
-		//id: 1,
-		//nombre: 'Computadora',
-		//descripcion: 'Computadora amd 5 nucleos',
-		//precio: 50000,
-	//},
-	//{
-		//id: 2,
-		//nombre: 'Laptop',
-		//descripcion: 'Laptop amd 5 nucleos',
-		//precio: 80000,
-	//},
-	//{
-		//id: 3,
-		//nombre: 'Celular',
-		//descripcion: 'Celular Samsung',
-		//precio: 60000,
-	//}
-//]
+let productos = [
+	{
+		id: 1,
+		nombre: 'Computadora',
+		descripcion: 'Computadora amd 5 nucleos',
+		precio: 50000,
+	},
+	{
+		id: 2,
+		nombre: 'Laptop',
+		descripcion: 'Laptop amd 5 nucleos',
+		precio: 80000,
+	},
+	{
+		id: 3,
+		nombre: 'Celular',
+		descripcion: 'Celular Samsung',
+		precio: 60000,
+	}
+]
+
+const btnBuscarProducto = document.getElementById("buscarProducto")
+btnBuscarProducto.addEventListener("click", ()=>{
+	let producABuscar = prompt("Ingrese el producto a buscar")
+	buscarProducto(producABuscar);
+
+})
+
+const btnBuscarProductoResultado = document.getElementById("buscarProductoResultado")
 
 
 
-const producto = ['Computadora', 'Laptop', 'Celular'];
-let carrito = [];
-
-function buscarProducto(producto){
-    for(i=0;i<carrito.length;i++) {
-        producto.toLowerCase();
-        if(producto == carrito[i]) {
-            productosBuscados.push(producto)
-        }
+function buscarProducto(producABuscar){
+    for(i=0;i<productos.length;i++) {
+        let busqueda = producABuscar.toLowerCase();
+		let resultado = productos[i].nombre.toLowerCase()
+		
+        if(busqueda == resultado) {
+			btnBuscarProductoResultado.textContent = `El resultado es: ${productos[i].nombre}, 
+			${productos[i].descripcion}, ${productos[i].precio} `
+			return productos[i]
+        }else{
+			btnBuscarProductoResultado.textContent = `Producto no encontrado `
+			return
+		}
     }
-	console.log(productosBuscados)
+	
 }
-buscarProducto(producto);
+
 
 
