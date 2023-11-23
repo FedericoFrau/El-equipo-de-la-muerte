c5ecfb4df45145eeea62bdb654228653bf852173
Ariana Alderete <alderete.ariana.1406@gmail.com>
2023-11-22 21:09:39 -0300

    quien sabe si andara

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-pepita la pistolera
+//let producto = [
+	//{
+		//id: 1,
+		//nombre: 'Computadora',
+		//descripcion: 'Computadora amd 5 nucleos',
+		//precio: 50000,
+	//},
+	//{
+		//id: 2,
+		//nombre: 'Laptop',
+		//descripcion: 'Laptop amd 5 nucleos',
+		//precio: 80000,
+	//},
+	//{
+		//id: 3,
+		//nombre: 'Celular',
+		//descripcion: 'Celular Samsung',
+		//precio: 60000,
+	//}
+//]
+
+
+
+const producto = ['Computadora', 'Laptop', 'Celular'];
+let carrito = [];
+
+function buscarProducto(producto){
+    for(i=0;i<carrito.length;i++) {
+        producto.toLowerCase();
+        if(producto == carrito[i]) {
+            productosBuscados.push(producto)
+        }
+    }
+	console.log(productosBuscados)
+}
+buscarProducto(producto);
+
+
